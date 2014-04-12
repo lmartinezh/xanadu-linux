6edd9828978b01c10da1f252bb5d57bed8f1e560
sinfallas <sinfallas@yahoo.com>
2014-04-12 16:57:41 -0430

    * se sustituye sysvinit por systemd * se agregaron nuevos paquetes a la lista negra * se agrego os-prober al script entorno * se modificaron los script para adaptarse al nuevo comando: systemctl * se agregaron parametros en failsafe para tarjetas radeon: radeon.modeset=0 xmodule=vesa * ahora se desactivan algunos servicios durante la construccion y no durante el arranque

--- config/includes.chroot/usr/lib/refractainstaller/pre-install/limpiar.sh
+++ config/includes.chroot/usr/lib/refractainstaller/pre-install/limpiar.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 #elaborado por sinfallas
 history -c
-service shorewall stop
-service fail2ban stop
-service psad stop
-service clamav-freshclam stop
-service i2p stop
-service tor stop
-service privoxy stop
-service polipo stop
-service cron stop
+systemctl stop avahi-daemon
+systemctl stop clamav-freshclam
+systemctl stop fail2ban
+systemctl stop i2p
+systemctl stop memlockd
+systemctl stop ntp
+systemctl stop onioncat
+systemctl stop polipo
+systemctl stop privoxy
+systemctl stop psad
+systemctl stop shorewall
+systemctl stop ssh
+systemctl stop tor
 service anacron stop
+service cron stop
 service rsyslog stop
-service avahi-daemon stop 
-service ntp stop
-service memlockd stop
 tsocks -off
 apt-get clean
 rm -rf /home/*/.mozilla/firefox/*/*Cache*
